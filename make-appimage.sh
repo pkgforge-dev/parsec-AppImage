f9c1fb70fc2c6cf3b4359b5936d5223b1b9c110e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-18 08:18:25 -0400

    fix icon path

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/parsecd.png
+export ICON=/usr/share/icons/hicolor/256x256/apps/parsecd.png
 export DESKTOP=/usr/share/applications/parsecd.desktop
 
 # Deploy dependencies
