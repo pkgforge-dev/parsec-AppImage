db65ae74b3eac106ae1f4d3b7bc82a243819228e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-19 21:07:55 -0400

    throw everything

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,9 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/parsecd.png
 export DESKTOP=/usr/share/applications/parsecd.desktop
+export DEPLOY_PIPEWIRE=1
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/parsecd /usr/share/parsec
